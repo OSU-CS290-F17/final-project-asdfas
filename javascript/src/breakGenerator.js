0008dd036fcb59bf8ca3485bc8f72365385cc986
ianbrown9475 <ian.brown9475@gmail.com>
2017-12-05 23:20:48 -0800

    Add remove buttons, break generator, dropdowns for inputs

---
+++ javascript/src/breakGenerator.js
@@ -0,0 +1,53 @@
+function hoursToString(hours) {
+  var str;
+  if(hours >= 10) {
+    str = Math.trunc(hours).toString();
+  }
+  else {
+    str = '0' + Math.trunc(hours).toString();
+  }
+  if(hours % 1) {
+    str = str + (hours % 1 * 60).toString();
+  }
+  else {
+    str = str + '00';
+  }
+  return str;
+}
+
+function breakGenerator(name, length, startTime, endTime) {
+  startTime = parseFloat(startTime);
+  endTime = parseFloat(endTime);
+  length = parseInt(length);
+  console.log('endTime:', endTime);
+  console.log('startTime:', startTime);
+  console.log('length:', length);
+  var numBreaks;
+  if(length === 30) {
+    numBreaks = (endTime - startTime) * 2;
+  }
+  else {
+    numBreaks = Math.trunc((endTime - startTime) * 2 - (length / 60));
+  }
+  // 'startTime': '12'
+  // 'endTime': '14'
+  // 'length': '30'
+  var breakObj = {};
+  breakObj[name] = {};
+  breakObj[name]['sections'] = {};
+
+  console.log('numBreaks:', numBreaks);
+  for(var i = 0; i < numBreaks; i++) {
+    var j = i.toString();
+    breakObj[name]['sections'][j] = {};
+    breakObj[name]['sections'][j]['length_minutes'] = length.toString();
+    var rangeStart = startTime + i * .5;
+    var rangeEnd = rangeStart + (length / 60);
+    rangeStart = hoursToString(rangeStart);
+    rangeEnd = hoursToString(rangeEnd);
+    breakObj[name]['sections'][j]['time_range'] = rangeStart + '-' + rangeEnd;
+  }
+  return breakObj;
+}
+
+export default breakGenerator;
